42593f334c1a5311af1ad566e82404aa697e75dd
bytoft2 <bytoft@live.dk>
2021-11-10 21:01:58 +0100

    Bottom Nav bar

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { DevicesScreen, HomeScreen } from './src/screens';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function App() {
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator 
+    initialRouteName="Home"
+    activeColor="#f0edf6"
+    inactiveColor="#3e2465"
+    barStyle={{ backgroundColor: '#694fad' }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} 
+      options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+      }}/>
+
+      <Tab.Screen name="Devices" component={DevicesScreen} 
+      options={{
+        tabBarLabel: "Devices",
+        tabBarIcon: ({color}) => (
+          <MaterialCommunityIcons name="format-list-bulleted" color={color} size= {26} />
+        )
+      }}/>
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/index.js
@@ -0,0 +1,3 @@
+export { default as DevicesScreen } from './DevicesScreen/DevicesScreen'
+
+export { default as HomeScreen } from './HomeScreen/HomeScreen'
